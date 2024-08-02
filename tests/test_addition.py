c5d79a8674c2ffbb12115b407b90c819f5f365eb
cherljoon <woul71@naver.com>
2024-08-02 17:04:33 +0900

    Add github action publish

---
+++ tests/test_addition.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+# tests/test_addition.py
+from packageuser34.addition import add
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(-1, -1) == -2
+    assert add(0, 0) == 0
